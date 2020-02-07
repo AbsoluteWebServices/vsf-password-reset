b922d76c07a3bf2cc0d911696bb1a8a30ec48b4f
Georgiy Slobodenyuk <georgiy@absoluteweb.com>
2020-02-07 14:59:40 -0800

    feat(multisite): Support multiple websites

--- API/password-reset/index.js
+++ API/password-reset/index.js
@@ -1,4 +1,5 @@
 import { apiStatus } from '../../../lib/util'
+import { multiStoreConfig } from '../../../platform/magento2/util'
 import { Router } from 'express'
 const Magento2Client = require('magento2-rest-client').Magento2Client
 
@@ -9,8 +10,10 @@ module.exports = ({ config, db }) => {
    * POST for creating new user's password
    */
   userApi.post('/create-password', (req, res) => {
-    const client = Magento2Client(config.magento2.api)
-    client.customers.resetPasswordUsingResetToken(req.body).then((result) => {
+    const { storeCode } = req.query
+    const websiteId = storeCode ? config.storeViews[storeCode].websiteId : undefined
+    const client = Magento2Client(multiStoreConfig(config.magento2.api, req))
+    client.customers.resetPasswordUsingResetToken({ ...req.body, websiteId }).then((result) => {
       apiStatus(res, result, 200)
     }).catch(err => {
       apiStatus(res, err, 500)
